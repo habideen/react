9e70d0752ab977ecb3077282f69ee07adbdd44a8
Habideen Ibrahim <habideenibrahim@gmail.com>
2023-08-10 04:58:50 +0100

    Add Router, Render house and heading to page

--- 1_react_practical_start/globomantics/src/main_page/index.js
+++ 1_react_practical_start/globomantics/src/main_page/index.js
@@ -1,12 +1,40 @@
-import logo from './logo.svg';
-import './main_page.css';
-import Header from './header';
+import { useEffect, useMemo, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./main_page.css";
+import Header from "./header";
+import FeaturedHouse from "./featured-house";
 
 function App() {
+  const [allHouses, setAllHouses] = useState([]);
+
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, []);
+
+  const featuredHouse = useMemo(() => {
+    if (allHouses.length) {
+      const randomIndex = Math.floor(Math.random() * allHouses.length);
+      return allHouses[randomIndex];
+    }
+  }, [allHouses]);
+
   return (
-    <div className='container'>
-      <Header subtitle="Providing houses all over the world" />
-    </div>
+    <Router>
+      <div className="container">
+        <Header subtitle="Providing houses all over the world" />
+
+        <Switch>
+          <Route path="/">
+            <FeaturedHouse house={featuredHouse}></FeaturedHouse>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
